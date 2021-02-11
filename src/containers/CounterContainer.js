4453532fab772d4828e4f5eba5f46525592646a4
ChanJuShin <ckswn1259@naver.com>
2021-02-11 13:20:49 +0900

    connect 함수

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import { decrease, increase, setDiff } from '../modules/counter';
 import Counter from '../components/Counter';
+import { bindActionCreators } from 'redux';
 
-function CounterContainer() {
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff
-  }));
-
-  const dispatch = useDispatch();
-  const onIncrease = () => dispatch(increase());
-  const onDecrease = () => dispatch(decrease());
-  const onSetDiff = (diff) => dispatch(setDiff(diff));
-
+function CounterContainer({ number, diff, increase, decrease, setDiff }) {
   return (
     <Counter
       number={number}
       diff={diff}
-      onIncrease={onIncrease}
-      onDecrease={onDecrease}
-      onSetDiff={onSetDiff}
+      onIncrease={increase}
+      onDecrease={decrease}
+      onSetDiff={setDiff}
     />
   );
 }
 
-export default CounterContainer;
+export default connect(
+  (state) => ({ number: state.counter.number, diff: state.counter.number }),
+  {
+    increase,
+    decrease,
+    setDiff
+  }
+)(CounterContainer);
